46723b44a976e0eaa3c914f8b26d5cd23206ca97
honza-rychtar <honza.rychtar@gmail.com>
2015-12-07 22:20:18 +0100

    Pridani dalsich knihoven

--- app/src/main/java/cz/mzk/kramerius/app/util/DomainUtil.java
+++ app/src/main/java/cz/mzk/kramerius/app/util/DomainUtil.java
@@ -19,37 +19,41 @@ public class DomainUtil {
 
             add(new Domain(true, "Moravská zemská knihovna", "http", "kramerius.mzk.cz", R.drawable.logo_mzk));
             add(new Domain(true, "Národní digitální knihovna", "http", "krameriusndktest.mzk.cz", R.drawable.logo_ndk));
-            add(new Domain(true, "Jihočeská vědecká knihovna v Českých Budějovicích", "http", "kramerius.cbvk.cz",
-                    R.drawable.logo_cbvk));
-            add(new Domain(true, "Vědecká knihovna v Olomouci", "http", "kramerius.kr-olomoucky.cz",
-                    R.drawable.logo_vkol));
-            add(new Domain(true, "Studijní a vědecká knihovna v Hradci Králové", "http", "kramerius4.svkhk.cz",
-                    R.drawable.logo_svkhk));
-            add(new Domain(true, "Městská knihovna Česká Třebová", "http", "k5.digiknihovna.cz",
-                    R.drawable.logo_mkct));
-            add(new Domain(true, "Severočeská vědecká knihovna v Ústí nad Labem", "http", "kramerius4.svkul.cz",
-                    R.drawable.logo_svkul));
-            add(new Domain(true, "Krajská knihovna Františka Bartoše ve Zlíně", "http", "dlib.kfbz.cz",
-                    R.drawable.logo_kfbz));
+            add(new Domain(true, "Jihočeská vědecká knihovna v Českých Budějovicích", "http", "kramerius.cbvk.cz", R.drawable.logo_cbvk));
+            add(new Domain(true, "Vědecká knihovna v Olomouci", "http", "kramerius.kr-olomoucky.cz", R.drawable.logo_vkol));
+            add(new Domain(true, "Studijní a vědecká knihovna v Hradci Králové", "http", "kramerius4.svkhk.cz", R.drawable.logo_svkhk));
+            add(new Domain(true, "Městská knihovna Česká Třebová", "http", "k5.digiknihovna.cz", R.drawable.logo_mkct));
+            add(new Domain(true, "Severočeská vědecká knihovna v Ústí nad Labem", "http", "kramerius4.svkul.cz", R.drawable.logo_svkul));
+            add(new Domain(true, "Krajská knihovna Františka Bartoše ve Zlíně", "http", "dlib.kfbz.cz", R.drawable.logo_kfbz));
 
-            add(new Domain(false, "Krajská knihovna Karlovy Vary", "http", "k4.kr-karlovarsky.cz", R.drawable.logo_kkkv));
-            add(new Domain(false, "Knihovna Akademie věd ČR (https)", "https", "kramerius.lib.cas.cz", R.drawable.logo_knav));
-            add(new Domain(false, "Knihovna Západočeského muzea v Plzni", "http", "kramerius.zcm.cz",
-                    R.drawable.logo_zcm));
-            add(new Domain(false, "Univerzita Karlova v Praze - Fakulta sociálních věd", "http",
-                    "kramerius.fsv.cuni.cz", R.drawable.logo_cuni_fsv));
+            add(new Domain(true, "Digitální studovna Ministerstva obrany ČR", "http", "kramerius.army.cz", R.drawable.logo_dsmo));
+            add(new Domain(true, "Národní technická knihovna", "http", "kramerius.techlib.cz", R.drawable.logo_ntk));
+            add(new Domain(true, "Národní filmový archiv", "http", "library.nfa.cz", R.drawable.logo_nfa));
+            add(new Domain(true, "Krajská knihovna Karlovy Vary", "http", "k4.kr-karlovarsky.cz", R.drawable.logo_kkkv));
+            add(new Domain(true, "Univerzita Karlova v Praze - Fakulta sociálních věd", "http", "kramerius.fsv.cuni.cz", R.drawable.logo_cuni_fsv));
+            add(new Domain(true, "Knihovna Akademie věd ČR", "http", "kramerius.lib.cas.cz", R.drawable.logo_knav));
+            add(new Domain(true, "Krajská vědecká knihovna v Liberci", "http", "kramerius.kvkli.cz", R.drawable.logo_kvkli));
+            add(new Domain(true, "Ústav zemědělské ekonomiky a informací", "http", "kramerius.uzei.cz", R.drawable.logo_uzei));
+
+
+            add(new Domain(false, "Moravskoslezská vědecká knihovna v Ostravě", "http", "camea.svkos.cz", R.drawable.logo_svkos));
+            add(new Domain(false, "Mendelova univerzita v Brně", "http", "kramerius4.mendelu.cz", R.drawable.logo_mendelu));
+            add(new Domain(false, "Židovské muzeum v Praze", "http", "kramerius4.jewishmuseum.cz", R.drawable.logo_zmp));
+            add(new Domain(false, "Národní muzeum", "http", "kramerius.nm.cz", R.drawable.logo_nm));
+            add(new Domain(false, "Slezská univerzita v Opavě", "http", "kramerius.slu.cz", R.drawable.logo_slu));
+            add(new Domain(false, "Lesnický a myslivecký digitální archiv", "http", "lmda.silvarium.cz", LOGO_DEFAULT));
+            add(new Domain(false, "Univerzitná knižnica v Bratislave", "http", "pc139.ulib.sk", LOGO_DEFAULT));
+            add(new Domain(false, "Výzkumný ústav geodetický, topografický a kartografický", "http", "knihovna-test.vugtk.cz", LOGO_DEFAULT));
+            add(new Domain(false, "Studijní a vědecká knihovna Plzeňského kraje", "http", "k4.svkpl.cz", R.drawable.logo_svkpk));
+
+            add(new Domain(false, "Knihovna Západočeského muzea v Plzni", "http", "kramerius.zcm.cz", R.drawable.logo_zcm));
             add(new Domain(false, "Městská knihovna v Praze", "http", "kramerius4.mlp.cz", R.drawable.logo_mlp));
-            // TODO: 26.10.15 Doplnit logo
-            add(new Domain(false, "Krajská vědecká knihovna v Liberci", "http", "kramerius.kvkli.cz", LOGO_DEFAULT));
             add(new Domain(false, "Národní knihovna", "http", "kramerius4.nkp.cz", R.drawable.logo_nkp));
-            add(new Domain(false, "Národní technická knihovna", "http", "kramerius.techlib.cz", R.drawable.logo_ntk));
-            add(new Domain(false, "Středočeská vědecká knihovna v Kladně", "http", "kramerius.svkkl.cz",
-                    R.drawable.logo_svkkl));
+            add(new Domain(false, "Středočeská vědecká knihovna v Kladně", "http", "kramerius.svkkl.cz", R.drawable.logo_svkkl));
             add(new Domain(false, "Česká digitální knihovna", "http", "cdk.lib.cas.cz", R.drawable.logo_cdk));
-            // add(new Domain("INCAD", "Test INCAD", "http", "sluzby.incad.cz/vmkramerius", R.drawable.logo_incad));
+
             add(new Domain(false, "Moravská zemská knihovna - Docker (https)", "https", "docker.mzk.cz", R.drawable.logo_mzk));
-            add(new Domain(false, "Moravská zemská knihovna - Demo", "http", "krameriusdemo.mzk.cz",
-                    R.drawable.logo_mzk));
+            add(new Domain(false, "Moravská zemská knihovna - Demo", "http", "krameriusdemo.mzk.cz", R.drawable.logo_mzk));
         }
     };
 
