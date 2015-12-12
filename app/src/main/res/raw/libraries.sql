fb9d1194d000305df948b74ce5077fda521fb2f1
honza-rychtar <honza.rychtar@gmail.com>
2015-12-13 00:06:39 +0100

    Doplneni lokalnich Krameriu

--- app/src/main/res/raw/libraries.sql
+++ app/src/main/res/raw/libraries.sql
@@ -6,3 +6,11 @@ INSERT INTO [library] ([name], [protocol], [domain], [code]) VALUES ('Studijní 
 INSERT INTO [library] ([name], [protocol], [domain], [code]) VALUES ('Městská knihovna Česká Třebová', 'http', 'k5.digiknihovna.cz', 'mkct');
 INSERT INTO [library] ([name], [protocol], [domain], [code]) VALUES ('Severočeská vědecká knihovna v Ústí nad Labem', 'http', 'kramerius.svkul.cz', 'svkul');
 INSERT INTO [library] ([name], [protocol], [domain], [code]) VALUES ('Krajská knihovna Františka Bartoše ve Zlíně', 'http', 'dlib.kfbz.cz', 'kfbz');
+INSERT INTO [library] ([name], [protocol], [domain], [code]) VALUES ("Digitální studovna Ministerstva obrany ČR", "http", "kramerius.army.cz", "dsmo");
+INSERT INTO [library] ([name], [protocol], [domain], [code]) VALUES ("Národní technická knihovna", "http", "kramerius.techlib.cz", "ntk");
+INSERT INTO [library] ([name], [protocol], [domain], [code]) VALUES ("Národní filmový archiv", "http", "library.nfa.cz", "nfa");
+INSERT INTO [library] ([name], [protocol], [domain], [code]) VALUES ("Krajská knihovna Karlovy Vary", "http", "k4.kr-karlovarsky.cz", "kkkv");
+INSERT INTO [library] ([name], [protocol], [domain], [code]) VALUES ("Univerzita Karlova v Praze - Fakulta sociálních věd", "http", "kramerius.fsv.cuni.cz", "cuni_fsv");
+INSERT INTO [library] ([name], [protocol], [domain], [code]) VALUES ("Knihovna Akademie věd ČR", "http", "kramerius.lib.cas.cz", "knav");
+INSERT INTO [library] ([name], [protocol], [domain], [code]) VALUES ("Krajská vědecká knihovna v Liberci", "http", "kramerius.kvkli.cz", "kvkli");
+INSERT INTO [library] ([name], [protocol], [domain], [code]) VALUES ("Knihovna Antonína Švehly", "http", "kramerius.uzei.cz", "uzei");
