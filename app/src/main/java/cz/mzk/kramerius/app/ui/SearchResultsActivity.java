1bdb6948e3440f8849c99be9cc0b724d9620c6fd
Jan Rychtar <honza.rychtar@gmail.com>
2017-01-20 06:31:30 +0100

    Naseptavac v toolbaru ve vysledcich hledani

--- app/src/main/java/cz/mzk/kramerius/app/ui/SearchResultsActivity.java
+++ app/src/main/java/cz/mzk/kramerius/app/ui/SearchResultsActivity.java
@@ -1,24 +1,42 @@
 package cz.mzk.kramerius.app.ui;
 
 import android.app.FragmentTransaction;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.TextView;
 
 import com.google.analytics.tracking.android.EasyTracker;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import cz.mzk.kramerius.app.BaseActivity;
 import cz.mzk.kramerius.app.OnItemSelectedListener;
 import cz.mzk.kramerius.app.R;
+import cz.mzk.kramerius.app.adapter.SearchSuggestionAdapter;
+import cz.mzk.kramerius.app.api.K5ConnectorFactory;
+import cz.mzk.kramerius.app.data.KrameriusContract;
 import cz.mzk.kramerius.app.model.Item;
 import cz.mzk.kramerius.app.search.Query;
+import cz.mzk.kramerius.app.util.Logger;
 import cz.mzk.kramerius.app.util.ModelUtil;
+import cz.mzk.kramerius.app.view.MaterialSearchView;
 
 public class SearchResultsActivity extends BaseActivity implements View.OnClickListener, OnItemSelectedListener, SearchFiltersFragment.FilterListener {
 
     public static final String EXTRA_QUERY = "extra_query";
+    private static final String LOG_TAG = SearchResultsActivity.class.getSimpleName();
 
 
     private SearchResultsFragment mSearchResultsFragment;
@@ -27,6 +45,10 @@ public class SearchResultsActivity extends BaseActivity implements View.OnClickL
     private boolean mFiltersMode;
     private TextView mSwitchBtn;
     private Query mQuery;
+
+    private MaterialSearchView mSearchView;
+    private int mSuggestionsIndex = 0;
+    private SearchSuggestionAdapter mSearchAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,6 +99,56 @@ public class SearchResultsActivity extends BaseActivity implements View.OnClickL
             mSearchFiltersFragment.setCallback(this);
         }
         setMode(false);
+
+
+
+
+        mSearchView = (MaterialSearchView) findViewById(R.id.search_view);
+        mSearchView.setVoiceSearch(true);
+        mSearchView.showVoice(true);
+        mSearchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Logger.debug(LOG_TAG, "SEARCH: onQueryTextSubmit");
+                onSearchQuery(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Logger.debug(LOG_TAG, "SEARCH: onQueryTextChange");
+                mSuggestionsIndex++;
+                //new GetSuggestionsTask(MainActivity.this, newText, mSuggestionsIndex).execute();
+                new SearchResultsActivity.GetSuggestionsTask(SearchResultsActivity.this, newText, mSuggestionsIndex).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+                return false;
+            }
+        });
+
+        mSearchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown() {
+                Logger.debug(LOG_TAG, "SEARCH: onSearchViewShown");
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+                Logger.debug(LOG_TAG, "SEARCH: onSearchViewClosed");
+            }
+        });
+        mSearchView.setSubmitOnClick(true);
+        mSearchAdapter = new SearchSuggestionAdapter(this);
+        mSearchView.setAdapter(mSearchAdapter);
+        mSearchView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Logger.debug(LOG_TAG, "setOnItemClickListener");
+                mSearchView.setQuery((String) mSearchAdapter.getItem(position), true);
+            }
+        });
+
+
+
+
     }
 
 
@@ -157,4 +229,109 @@ public class SearchResultsActivity extends BaseActivity implements View.OnClickL
             switchMode();
         }
     }
+
+
+
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        MenuItem item = menu.findItem(R.id.action_search);
+        mSearchView.setMenuItem(item);
+
+        return true;
+    }
+
+
+    class GetSuggestionsTask extends AsyncTask<String, Void, List<String>> {
+
+        private Context tContext;
+        private String tQuery;
+        private int tIndex;
+
+        public GetSuggestionsTask(Context context, String query, int index) {
+            tContext = context;
+            tQuery = query;
+            if (tQuery != null) {
+                tQuery = tQuery.trim();
+            }
+            tIndex = index;
+        }
+
+        @Override
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected List<String> doInBackground(String... params) {
+            if (tQuery == null || tQuery.length() < 1) {
+                return new ArrayList<String>();
+            }
+            return K5ConnectorFactory.getConnector().getSuggestions(tContext, tQuery, 30);
+        }
+
+        @Override
+        protected void onPostExecute(List<String> suggestions) {
+            // mSuggestionsLoading = false;
+            if (tContext == null || mSearchView == null || suggestions == null || tIndex < mSuggestionsIndex) {
+                return;
+            }
+            Logger.debug(LOG_TAG, "setting suggestions: " + suggestions);
+            List<String> s = new ArrayList<>();
+            Cursor c = getContentResolver().query(KrameriusContract.SearchEntry.CONTENT_URI,
+                    new String[]{ KrameriusContract.SearchEntry.COLUMN_QUERY },
+                    KrameriusContract.SearchEntry.COLUMN_QUERY + " LIKE ? ",
+                    new String[]{ tQuery + "%" },
+                    KrameriusContract.SearchEntry.COLUMN_TIMESTAMP + " DESC");
+            while(c.moveToNext()) {
+                s.add(c.getString(0));
+            }
+            c.close();
+            mSearchAdapter.refresh(tQuery, s, suggestions);
+            if (mSearchAdapter.getCount() > 0) {
+                mSearchView.showSuggestions();
+            } else {
+                mSearchView.dismissSuggestions();
+            }
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == MaterialSearchView.REQUEST_VOICE && resultCode == RESULT_OK) {
+            ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            if (matches != null && matches.size() > 0) {
+                String searchWrd = matches.get(0);
+                if (!TextUtils.isEmpty(searchWrd)) {
+                    mSearchView.dismissSuggestions();
+                    mSearchView.setQuery(searchWrd, true);
+                }
+            }
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+
+    public void onSearchQuery(String query) {
+        if (query == null || query.length() == 0) {
+            return;
+        }
+        ContentValues cv = new ContentValues();
+        cv.put(KrameriusContract.SearchEntry.COLUMN_QUERY, query);
+        cv.put(KrameriusContract.SearchEntry.COLUMN_TIMESTAMP, System.currentTimeMillis());
+        getContentResolver().insert(KrameriusContract.SearchEntry.CONTENT_URI, cv);
+
+        Intent intent = new Intent(SearchResultsActivity.this, SearchResultsActivity.class);
+        intent.putExtra(SearchResultsActivity.EXTRA_QUERY, query);
+        startActivity(intent);
+        finish();
+    }
+
 }
